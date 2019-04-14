7dec706fba0f88df8848a29af4b690c3b1bda575
karvozavr <karvozavr@gmail.com>
2019-04-14 19:59:17 +0300

    Refactor Character class
    Add @dataclass and explicit fields declaration. Move initialisations to base class.

--- game/model/character.py
+++ game/model/character.py
@@ -1,21 +1,31 @@
 from abc import ABC
+from typing import List, Tuple
 
+from game.elements import Artifact
 from game.model.entity import Entity
+from dataclasses import dataclass
 
 
+@dataclass
 class Character(Entity, ABC):
     """
     Base character class.
     """
 
-    def __init__(self, position):
+    max_health: int
+    health: int
+    gold: int
+    experience: int
+    items: List[Artifact]
+    items_limit: int
+
+    def __init__(self, position: Tuple[int, int], max_health=5, items_limit=10):
         super().__init__(position)
-        self.max_health = 5
-        self.health = 5
+        self.max_health = max_health
+        self.health = max_health
         self.gold = 0
         self.experience = 0
         self.items = []
-        self.items_limit = 10
 
     def move(self, x, y):
         """
@@ -32,16 +42,15 @@ class Hero(Character):
     """
     Hero is the character controlled by the player.
     """
+
     def __init__(self, position):
-        super().__init__(position)
-        self.max_health = 7
-        self.health = 7
+        super().__init__(position, max_health=7)
 
 
 class Mob(Character):
     """
     Mob, the enemy to the player.
     """
+
     def __init__(self, position):
-        super().__init__(position)
-        self.items_limit = 3
+        super().__init__(position, items_limit=3)
