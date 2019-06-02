cc1a9e8a3453687f11cb8b25fd88bc22308b2d7a
karvozavr <karvozavr@gmail.com>
2019-06-02 16:29:34 +0300

    Add Server prototype and protobuf config

---
+++ game/server/server.py
@@ -0,0 +1,22 @@
+from concurrent import futures
+import grpc
+
+from .generated import progue_pb2_grpc, progue_pb2
+
+
+class ProgueServer(progue_pb2_grpc.ProgueServerServicer):
+    def GetState(self, request, context):
+        return progue_pb2.State()
+
+    def MakeTurn(self, request, context):
+        if request.action_type == 1:
+            pass
+
+        return progue_pb2.MakeTurnResponse()
+
+
+def start_server():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=50))
+    progue_pb2_grpc.add_ProgueServerServicer_to_server(ProgueServer(), server)
+    server.add_insecure_port('[::]:51051')
+    server.start()
