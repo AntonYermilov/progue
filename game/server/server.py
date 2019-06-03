30e6e425532449a6ca432dcfa261e116aa816cc2
karvozavr <karvozavr@gmail.com>
2019-06-03 11:06:42 +0300

    Add server object

--- game/server/server.py
+++ game/server/server.py
@@ -14,9 +14,14 @@ class ProgueServer(progue_pb2_grpc.ProgueServerServicer):
 
         return progue_pb2.MakeTurnResponse()
 
+    def server_loop(self):
+        pass
+
 
 def start_server():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=50))
-    progue_pb2_grpc.add_ProgueServerServicer_to_server(ProgueServer(), server)
+    server_object = ProgueServer()
+    progue_pb2_grpc.add_ProgueServerServicer_to_server(server_object, server)
     server.add_insecure_port('[::]:51051')
     server.start()
+
