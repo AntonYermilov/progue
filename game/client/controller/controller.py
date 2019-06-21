5dda57be215d9659d5f7046b5dd34fde7bf9a0ee
AntonYermilov <anton.yermilov@gmail.com>
2019-06-21 14:12:37 +0300

    added logging

--- game/client/controller/controller.py
+++ game/client/controller/controller.py
@@ -1,5 +1,6 @@
 import json
-import sys
+import logging
+from datetime import datetime
 from pathlib import Path
 from typing import Union
 
@@ -15,6 +16,7 @@ class Controller:
     FRAMES_PER_SECOND = 20
     GAME_CONFIG_PATH = Path('resources', 'config', 'game_config.json')
     ENTITIES_CONFIG_PATH = Path('resources', 'config', 'entities.json')
+    LOG_DIR_PATH = Path('resources', 'logs')
 
     def __init__(self, *args, **kwargs):
         with self.GAME_CONFIG_PATH.open('r') as src:
@@ -25,6 +27,25 @@ class Controller:
         self.menu = None
         self.view = View(self, self.model, self.entities_desc)
 
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.logger.setLevel(logging.DEBUG)
+        self._create_log_handler()
+
+    def _create_log_handler(self):
+        if not Controller.LOG_DIR_PATH.exists():
+            Controller.LOG_DIR_PATH.mkdir()
+
+        current_date = datetime.now().strftime('%Y.%m.%d %H.%M.%S')
+        log_name = 'client {}.txt'.format(current_date)
+        log_file = Controller.LOG_DIR_PATH / log_name
+
+        file_handler = logging.FileHandler(str(log_file))
+        file_handler.setLevel(logging.INFO)
+
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', '%m/%d/%Y %I:%M:%S %p')
+        file_handler.setFormatter(formatter)
+        self.logger.addHandler(file_handler)
+
     """
     Starts the game on the client side.
     Processes all user actions and interacts with server. 
@@ -33,24 +54,33 @@ class Controller:
         self.view.create()
         error = None
 
-        while True:
-            self.view.initialize()
+        self.logger.info('Game started')
 
+        while True:
+            self.logger.info('On new game stage')
+            self.view.initialize()
             self.menu = Menu(self.view, error)
             error = None
 
             try:
+                self.logger.info('On make_choice stage')
                 network = self.menu.make_choice()
                 if network is None:
+                    self.logger.info('No network received, possible exit button was clicked')
                     break
+                self.logger.info(f'Network was successfully created, singleplayer mode: {network.singleplayer}')
 
                 if not network.singleplayer:
                     self.view.set_game_id(network.game_id)
                 else:
                     self.view.set_game_id(None)
 
+                self.logger.info('Starting game loop')
                 while True:
+                    self.logger.info('Receiving game state...')
                     state = network.get_state()
+                    self.logger.info('Success')
+
                     self.model.update(state)
                     self.view.refresh_game()
 
@@ -73,9 +103,11 @@ class Controller:
                                 break
 
                     self.view.delay(1.0 / self.FRAMES_PER_SECOND)
+                self.logger.info('Game successfully finished')
             except Exception as e:
                 error = 'Disconnected from server'
-                print(e, file=sys.stderr)
+                self.logger.error('Disconnected from server')
+                self.logger.exception(e)
             finally:
                 self.menu.destroy()
         self.view.destroy()
